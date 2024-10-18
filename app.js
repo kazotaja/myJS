6fbe7228eb0fee7d471df9fa1270fecf8c3b9abd
kazotaja <kazotaja@gmail.com>
2024-10-18 11:11:58 +0300

    save first page files

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const btnHamburger = document.querySelector('.hamburger');
 const menuPanel = document.querySelector('.menu-panel')
 const menuLinks = document.querySelectorAll('.menu-panel a')
+const frameImage = document.querySelector('#frame')
+const images = ['1.webp', '2.webp', '3.webp', '4.webp', '5.webp', '6.webp'];
+
+let currentImageIndex = 0;
+
+frameImage.addEventListener('click', ()=> {
+    currentImageIndex++; //+1
+
+    if(currentImageIndex >= 5) {
+        currentImageIndex = 0;
+    }
+    
+    frameImage.src = "images/" + images[currentImageIndex]; 
+})
 
 btnHamburger.addEventListener('click', ()=> {
     menuPanel.classList.toggle('active'); 
